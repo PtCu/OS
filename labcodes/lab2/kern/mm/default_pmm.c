baba29958b851a88f0917f3e37e025e8f02a74f4
PtCu <1365699009@qq.com>
2020-12-03 18:47:06 +0800

    1

--- labcodes/lab2/kern/mm/default_pmm.c
+++ labcodes/lab2/kern/mm/default_pmm.c
@@ -60,92 +60,164 @@ free_area_t free_area;
 #define nr_free (free_area.nr_free)
 
 static void
-default_init(void) {
+default_init(void)
+{
     list_init(&free_list);
     nr_free = 0;
 }
 
 static void
-default_init_memmap(struct Page *base, size_t n) {
+default_init_memmap(struct Page *base, size_t n)
+{
     assert(n > 0);
     struct Page *p = base;
-    for (; p != base + n; p ++) {
+    /*
+    遍历所有空闲物理页的Page结构，
+    将Page结构的描述空闲块的数目的成员变量置零
+    （因此该成员变量只有在整个空闲块的第一个Page中才有意义），
+    然后清空这些物理页的引用计数，然后通过设置flags的位的方式将其标记为空闲，
+    */
+    for (; p != base + n; p++)
+    {
         assert(PageReserved(p));
         p->flags = p->property = 0;
         set_page_ref(p, 0);
     }
+    /*
+    接下来对空闲块的第一个页的Page结构进行初始化，
+    具体实现为将其表示空闲块大小的成员变量设置为作为参数传入的空闲块大小（单位为页），
+    然后更新存储所有空闲页数量的全局变量，然后将这个空闲块插入到空闲内存块链表中
+    （只需要将第一个Page的page_link插入即可）
+    */
     base->property = n;
     SetPageProperty(base);
     nr_free += n;
     list_add(&free_list, &(base->page_link));
 }
 
+/*
+该函数的具体功能为分配指定页数的连续空闲物理空间，
+并且将第一页的Page结构的指针作为结果返回；该函数的具体实现方式如下：
+*/
 static struct Page *
-default_alloc_pages(size_t n) {
+default_alloc_pages(size_t n)
+{
+    //对参数进行合法性检查，并且查询总的空闲物理页数目是否足够进行分配，如果不足够进行分配，直接返回NULL，表示分配失败；
     assert(n > 0);
-    if (n > nr_free) {
+    if (n > nr_free)
+    {
         return NULL;
     }
     struct Page *page = NULL;
-    list_entry_t *le = &free_list;
-    while ((le = list_next(le)) != &free_list) {
-        struct Page *p = le2page(le, page_link);
-        if (p->property >= n) {
-            page = p;
-            break;
+    list_entry_t *le,*len; //当前list entry和下一个list entry
+    le = &free_list;
+    /*
+    从头开始遍历保存空闲物理内存块的链表(按照物理地址的从小到大顺序)，
+    如果找到某一个连续内存块的大小不小于当前需要的连续内存块大小，
+    则说明可以进行成功分配（选择第一个遇到的满足条件的空闲内存块来完成内存分配）
+    如果该内存块的大小大于需要的内存大小，则将空闲内存块分裂成两块，
+    物理地址较小的一块分配出来进行使用（大小恰好为需要的物理内存的大小），
+    而物理地址较大的那一块重新进行初始化（包括对第一个Page中表示空闲块大小的成员变量进行设置，
+    其应当设置为原先的空闲块大小减掉分配掉的大小，
+    以及将这个分裂出来的空闲块插入到空闲块链表中（该链表中的空闲块按照物理地址从小到大排序））；
+    如果原先的空闲块大小刚好等于需要的内存大小，则没有比较进行分裂；
+    于此同时，对分配出去的物理内存的每一个的描述信息（即对应的Page结构）进行初始化，
+    具体为修改flags成员变量来将这些Page标记为非空闲，最后将原始空闲块在空闲块链表中删除掉，
+    并且更新表示总空闲页数量的全局变量；最后用于表示分配到的物理内存的Page结构指针返回。
+    */
+    while((le=list_next(le)) != &free_list) {
+      struct Page *p = le2page(le, page_link);
+      if(p->property >= n){ //如果找到一个足够大的空闲块，
+        int i;
+        for(i=0;i<n;i++){  //依次向后遍历n个page，
+          len = list_next(le);
+          struct Page *pp = le2page(le, page_link);
+          SetPageReserved(pp);//并将这些page标记为已占用
+          ClearPageProperty(pp); //清空property位，表示这些page不是头page
+          list_del(le); //从空闲列表删除
+          le = len;
         }
-    }
-    if (page != NULL) {
-        list_del(&(page->page_link));
-        if (page->property > n) {
-            struct Page *p = page + n;
-            p->property = page->property - n;
-            list_add(&free_list, &(p->page_link));
-    }
+        if(p->property>n){  //如果还有剩余的，就修改page头的剩余量
+          (le2page(le,page_link))->property = p->property - n;
+        }
+        ClearPageProperty(p); //头page为0表示已经被分配
+        SetPageReserved(p); //把page标记为已占用
         nr_free -= n;
-        ClearPageProperty(page);
+        return p;
+        //最后结果是满足大小的若干个page从空闲列表中删去，并且设置相应标记位
+      }
     }
-    return page;
+    return NULL;
+   
 }
-
+/*
+释放指定的某一物理页开始的若干个连续物理页，
+并且完成first-fit算法中需要的若干信息的维护，
+*/
 static void
-default_free_pages(struct Page *base, size_t n) {
+default_free_pages(struct Page *base, size_t n)
+{
+    //确定是否合法，page是否真的被占用
     assert(n > 0);
-    struct Page *p = base;
-    for (; p != base + n; p ++) {
-        assert(!PageReserved(p) && !PageProperty(p));
-        p->flags = 0;
-        set_page_ref(p, 0);
+    assert(PageReserved(base));
+
+    //从free_list开始找，找到合适插入base的地方（找到地址比base高的块）为止
+    list_entry_t *le = &free_list;
+    struct Page * p;
+    while((le=list_next(le)) != &free_list) {
+      p = le2page(le, page_link);
+      if(p>base){
+        break;
+      }
     }
-    base->property = n;
+    //插入算是base一共n个的空闲块
+    //list_add_before(le, base->page_link);
+    for(p=base;p<base+n;p++){
+      list_add_before(le, &(p->page_link));
+    }
+    //将base标记设置为空闲
+    base->flags = 0;
+    set_page_ref(base, 0);
+    ClearPageProperty(base);
     SetPageProperty(base);
-    list_entry_t *le = list_next(&free_list);
-    while (le != &free_list) {
-        p = le2page(le, page_link);
-        le = list_next(le);
-        if (base + base->property == p) {
-            base->property += p->property;
-            ClearPageProperty(p);
-            list_del(&(p->page_link));
-        }
-        else if (p + p->property == base) {
-            p->property += base->property;
-            ClearPageProperty(base);
-            base = p;
-            list_del(&(p->page_link));
-        }
+    base->property = n;
+    
+    //此时le为base后面的page。
+    p = le2page(le,page_link) ;
+    //和后面的相邻空闲块进行合并
+    if( base+n == p ){
+      base->property += p->property;
+      p->property = 0;
     }
+    //和前面的相邻块合并
+    le = list_prev(&(base->page_link));
+    p = le2page(le, page_link);
+    if(le!=&free_list && p==base-1){
+      while(le!=&free_list){
+        //如果是空闲的就把base合并到前面去
+        if(p->property){
+          p->property += base->property;
+          base->property = 0;
+          break;
+        }
+        le = list_prev(le);
+        p = le2page(le,page_link);
+      }
+    }
+
     nr_free += n;
-    list_add(&free_list, &(base->page_link));
+    return ;
 }
 
 static size_t
-default_nr_free_pages(void) {
+default_nr_free_pages(void)
+{
     return nr_free;
 }
 
 static void
-basic_check(void) {
+basic_check(void)
+{
     struct Page *p0, *p1, *p2;
     p0 = p1 = p2 = NULL;
     assert((p0 = alloc_page()) != NULL);
@@ -195,16 +267,18 @@ basic_check(void) {
     free_page(p2);
 }
 
-// LAB2: below code is used to check the first fit allocation algorithm (your EXERCISE 1) 
+// LAB2: below code is used to check the first fit allocation algorithm (your EXERCISE 1)
 // NOTICE: You SHOULD NOT CHANGE basic_check, default_check functions!
 static void
-default_check(void) {
+default_check(void)
+{
     int count = 0, total = 0;
     list_entry_t *le = &free_list;
-    while ((le = list_next(le)) != &free_list) {
+    while ((le = list_next(le)) != &free_list)
+    {
         struct Page *p = le2page(le, page_link);
         assert(PageProperty(p));
-        count ++, total += p->property;
+        count++, total += p->property;
     }
     assert(total == nr_free_pages());
 
@@ -252,9 +326,10 @@ default_check(void) {
     free_pages(p0, 5);
 
     le = &free_list;
-    while ((le = list_next(le)) != &free_list) {
+    while ((le = list_next(le)) != &free_list)
+    {
         struct Page *p = le2page(le, page_link);
-        count --, total -= p->property;
+        count--, total -= p->property;
     }
     assert(count == 0);
     assert(total == 0);
@@ -269,4 +344,3 @@ const struct pmm_manager default_pmm_manager = {
     .nr_free_pages = default_nr_free_pages,
     .check = default_check,
 };
-
